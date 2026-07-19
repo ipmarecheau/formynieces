5db3815fabe4eea302c66cb6adbc242b8edab6ef
Isaac Marecheau <purnell07@gmail.com>
2026-07-19 01:35:32 -0400

    fix: mount database to separate db directory so seeders remain accessible

--- deploy.sh
+++ deploy.sh
@@ -34,7 +34,7 @@ docker run -d \
   --name formynieces \
   --restart unless-stopped \
   -p 8080:8080 \
-  -v /opt/formynieces-data/database:/var/www/html/database \
+  -v /opt/formynieces-data/db:/var/www/html/db \
   -v /opt/formynieces-data/storage:/var/www/html/storage \
   formynieces:latest
 
